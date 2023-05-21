7eb005c3f1bfc2a4172d8f2c20dec62110e9369c
Niels <git@nierot.com>
2023-05-21 13:39:43 +0200

    add class-validator

--- src/song/create-song.dto.ts
+++ src/song/create-song.dto.ts
@@ -1,11 +1,29 @@
+import { IsArray, IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 import { CreateArtistDto } from 'src/artist/create-artist.dto'
+import { Platform } from './song.entity'
 
 export class CreateSongDto {
+  @IsNotEmpty()
   title: string
+
+  @IsNotEmpty()
+  @IsArray()
   artists: CreateArtistDto[]
+
+  @IsNotEmpty()
   album: string
+
+  @IsNotEmpty()
   length: number
-  image: string
-  platform: string
+
+  @IsOptional()
+  image: string | null
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEnum(Platform)
+  platform: Platform
+
+  @IsNotEmpty()
   platformId: string
 }
